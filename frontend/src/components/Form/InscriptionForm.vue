acdd0b210439fb7a231c251bce33c96e3df78976
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2021-08-25 16:35:45 +0200

    test ok signup front+back ok

--- frontend/src/components/Form/InscriptionForm.vue
+++ frontend/src/components/Form/InscriptionForm.vue
@@ -6,42 +6,37 @@
     v-model="valid"
     lazy-validation
   >
-  <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      prepend-icon="mdi-email-edit-outline"
-      required
-    ></v-text-field>
 
-    <v-text-field
-      v-model="name"
+  <v-text-field
+      v-model="dataSignup.username"
       :counter="10"
-      :rules="nameRules"
+     
       label="Pseudo"
       prepend-icon="mdi-format-text-variant-outline"
       required
     ></v-text-field>
 
+  <v-text-field
+      v-model="dataSignup.email"
+      
+      label="E-mail"
+      prepend-icon="mdi-email-edit-outline"
+      required
+    ></v-text-field>
+
+
+
       <v-text-field
-            v-model="password"
+            v-model="dataSignup.password"
             :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]"
-            :type="show1 ? 'text' : 'password'"
             name="input-15-5"
             label="Mot de passe"
-            hint="Minimum 8 caractére"
+            hint="Minimum 8 caractére au moins une lettre minuscule, une lettre majuscule, un chiffre et un de ces caractères spéciaux: $ @ % * + - _ !"
             prepend-icon="mdi-form-textbox-password"
             counter
             @click:append="show1 = !show1"
           ></v-text-field>
 
-     <v-file-input
-    id="inputFile"
-    accept="image/*"
-    label="image"
-    @change="onFileChanged"
-  ></v-file-input>
 
     <v-btn
      
@@ -84,66 +79,53 @@
 
 <script>
 import axios from "axios";
-  export default {
-      name: 'InscriptionForm',
-    data () {
-      return {
-        email: '',
-        emailRules: [
-        v => !!v || 'E-mail est obligatoire',
-        v => /.+@.+\..+/.test(v) || 'L\'email doit être valide',
-      ],
-        name: '',
-        show1: false,
-        password: '',
-        rules: {
-          required: value => !!value || 'Obligatoire',
-          min: v => v.length >= 8 || 'Minimum 8 caractére',
-          emailMatch: () => (`L'email et le mot de passe que vous avez entrés ne correspondent pas`),
+export default {
+    name: "InscriptionForm",
+    data() {
+        return {
+            dataSignup: {
+                username: null,
+                email: null,
+                password: null,
+            },
+            msg: "",
+        };
+    },
+    methods: {
+        sendSignup() {
+            const regexPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+            const regexEmail = /^[a-z0-9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
+            if (
+                (this.dataSignup.email !== null ||
+                    this.dataSignup.username !== null ||
+                    this.dataSignup.password !== null) &&
+                regexPassword.test(this.dataSignup.password) &&
+                regexEmail.test(this.dataSignup.email) &&
+                this.dataSignup.username
+            ) {
+                axios
+                    .post(
+                        "http://localhost:3000/api/auth/signup",
+                        this.dataSignup
+                    )
+                    .then((response) => {
+                        console.log(response);
+                        // this.dataSignup.email = null;
+                        // this.dataSignup.username = null;
+                        // this.dataSignup.password = null;
+                        location.replace(location.origin);
+                    })
+                    .catch((error) => console.log(error));
+            } else {
+                alert(
+                    "Le mot de passe doit contenir de 8 à 15 caractères,au moins une lettre minuscule, une lettre majuscule, un chiffre et un de ces caractères spéciaux: $ @ % * + - _ !"
+                );
+            }
         },
-          selectedFile: '',
-
-          }
     },
-
-         methods: {
-      sendSignup () {
-        const formData = new FormData();
-  formData.append('name', this.name);
-  formData.append('email', this.email);
-  formData.append('password', this.password);
-  formData.append('inputFile', this.selectedFile);
-if (formData.get("email") !== null & formData.get("name") !== null & formData.get("password") !== null) 
- { this.msg ="ERREUR DE SAISIE"}
-
-  {
-        axios
-          .post("http://localhost:3000/api/auth/signup", formData)
-          .then(response => {
-            console.log(response); //une fois le compte enregistré on remet les inputs "à 0"
-            //Réinitialisation
-            this.email = null;
-            this.name = null;
-            this.password = null;
-            document. location. href="http://localhost:8080/login";
-          })
-          .catch(error => console.log(error));
-      } 
-    
-        
-      },
-
-       onFileChanged (event) { //me permet de charger un fichier (une image) au click
-    this.dataSignup.selectedFile = event.target.files[0];
-    console.log(this.dataSignup.selectedFile)
-  },
-      reset () {
-        this.$refs.form.reset()
-      },
-      
-    },
+    };
      
-  }
+  
 
   
 </script>
