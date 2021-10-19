f9fdf43b0569f0b695a4ed0843ffe34565a5d245
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2021-10-19 18:57:50 +0200

    maj responsive

--- src/components/CardProfil.vue
+++ src/components/CardProfil.vue
@@ -1,85 +1,82 @@
 <template>
-  <v-card class="" max-width="200">
-    <v-img height="100%" src="../assets/black.jpg">
-      <v-row align="end" class="fill-height">
-        <v-col align-self="start" class="pa-0 mt-6 ml-6" cols="12">
-          <v-avatar class="profile" color="grey" size="80%" tile>
-            <v-img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShL2QkLAbENIkAN3QthsNkPpeKXRAPSnigtEgzPjvzvGTS3TtjJXZryy891VgTNqrN8MI&usqp=CAU"
-            ></v-img>
-          </v-avatar>
-        </v-col>
-        <v-col class="py-0 text-center">
-          <v-list-item color="white" dark>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6 mb-5">
-                {{ user.username }}
-              </v-list-item-title>
-              <v-list-tile-sub-title> {{ user.email }}</v-list-tile-sub-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider class="my-1"></v-divider>
-          <v-btn
-            class="button"
-            depressed
-            rounded
-            text
-            @click.prevent="MyProfil"
-            color="white"
-          >
-            Mon profil
-          </v-btn>
-          <v-divider class="my-1"></v-divider>
-          <v-btn
-            class="button"
-            depressed
-            rounded
-            text
-            v-if="user.isAdmin == true"
-            @click.prevent="Users"
-            color="white"
-          >
-            Tous les profil
-          </v-btn>
-          <v-divider class="my-1"></v-divider>
-          <v-btn
-            class="button"
-            depressed
-            rounded
-            text
-            v-if="user.isAdmin == true"
-            @click.prevent="Actual"
-            color="white"
-          >
-            Fil d'actualité
-          </v-btn>
-          <v-divider class="my-1"></v-divider>
+  <v-row>
+    <v-col cols="12">
+      <v-card class="card" width="200">
+        <v-img height="auto" src="../assets/black.jpg">
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0 mt-6 ml-6 " cols="12">
+              <v-avatar class="profile" color="grey" size="80%" tile>
+                <v-img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShL2QkLAbENIkAN3QthsNkPpeKXRAPSnigtEgzPjvzvGTS3TtjJXZryy891VgTNqrN8MI&usqp=CAU"
+                ></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="py-0 text-center" cols="12">
+              <v-list-item color="white" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h6 mb-5">
+                    {{ user.username }}
+                  </v-list-item-title>
+                  <v-list-tile-sub-title> {{ user.email }}</v-list-tile-sub-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider class="my-1"></v-divider>
+              <v-btn class="button" depressed rounded text @click.prevent="MyProfil" color="white">
+                Mon profil
+              </v-btn>
+              <v-divider class="my-1"></v-divider>
+              <v-btn
+                class="button"
+                depressed
+                rounded
+                text
+                v-if="user.isAdmin == true"
+                @click.prevent="Users"
+                color="white"
+              >
+                Tous les profil
+              </v-btn>
+              <v-divider class="my-1"></v-divider>
+              <v-btn
+                class="button"
+                depressed
+                rounded
+                text
+                v-if="user.isAdmin == true"
+                @click.prevent="Actual"
+                color="white"
+              >
+                Fil d'actualité
+              </v-btn>
+              <v-divider class="my-1"></v-divider>
 
-          <v-btn
-            class="button"
-            depressed
-            rounded
-            text
-            @click.prevent="createMessage"
-            color="white"
-          >
-            Crée Message
-          </v-btn>
-          <v-divider class="my-1"></v-divider>
-          <v-btn
-            depressed
-            rounded
-            text
-            @click.prevent="disconect"
-            color="white"
-            class="mb-10 button"
-          >
-            déconnection
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-img>
-  </v-card>
+              <v-btn
+                class="button"
+                depressed
+                rounded
+                text
+                @click.prevent="createMessage"
+                color="white"
+              >
+                Crée Message
+              </v-btn>
+              <v-divider class="my-1"></v-divider>
+              <v-btn
+                depressed
+                rounded
+                text
+                @click.prevent="disconect"
+                color="white"
+                class="mb-10 button"
+              >
+                déconnection
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -87,17 +84,17 @@ import axios from "axios";
 export default {
   data() {
     return {
-      user: "",
+      user: ""
     };
   },
 
   created() {
     axios
       .get("http://localhost:3000/api/auth/", {
-        headers: { Authorization: "Bearer " + localStorage.token },
+        headers: { Authorization: "Bearer " + localStorage.token }
       })
-      .then((response) => (this.user = response.data.user))
-      .catch((err) => console.log(err));
+      .then(response => (this.user = response.data.user))
+      .catch(err => console.log(err));
   },
 
   methods: {
@@ -116,8 +113,8 @@ export default {
     },
     Users() {
       document.location.href = "http://localhost:8080/users";
-    },
-  },
+    }
+  }
 };
 </script>
 
--- src/components/Header/HeaderHome.vue
+++ src/components/Header/HeaderHome.vue
@@ -1,35 +1,65 @@
 <template>
   <v-app-bar app color="teal darken-2" dark>
-    <div class="d-flex align-center">
-      <v-img
-        alt="logo groupomania"
-        class="logo"
-        contain
-        min-width="100"
-        src="../logo/icon-left-font-monochrome-white.png"
-        width="400"
-      />
-    </div>
-    <div id="btn">
-      <v-btn class="button">
-        <router-link class="lien" to="/signup">Inscription</router-link>
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
+    <v-col cols="7" xs="8" md="6">
+      <div class="d-flex align-center">
+        <v-img
+          alt="logo groupomania"
+          class="logo"
+          contain
+          min-width="100"
+          src="../logo/icon-left-font-monochrome-white.png"
+          width="400"
+        />
+      </div>
+    </v-col>
+    <v-col>
+      <div id="btn" class="hidden-sm-only hidden-xs-only">
+        <v-btn class="button">
+          <router-link class="lien" to="/signup">Inscription</router-link>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
 
-      <v-btn class="button">
-        <router-link class="lien" to="/login">Connexion</router-link>
-        <v-icon>mdi-lock-open-variant</v-icon>
-      </v-btn>
+        <v-btn class="button">
+          <router-link class="lien" to="/login">Connexion</router-link>
+          <v-icon>mdi-lock-open-variant</v-icon>
+        </v-btn>
+      </div>
+    </v-col>
+    <div class="hidden-md-and-up">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn dark v-on="on">
+            Dropdown
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>
+              <v-btn color="teal darken-2" class="button">
+                <router-link class="lien" to="/signup">Inscription</router-link>
+                <v-icon>mdi-account</v-icon>
+              </v-btn>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              <v-btn color="teal darken-2" class="button">
+                <router-link class="lien" to="/login">Connexion</router-link>
+                <v-icon>mdi-lock-open-variant</v-icon>
+              </v-btn></v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </div>
   </v-app-bar>
 </template>
 
 <script>
 export default {
-  name: "HeaderHome",
+  name: "HeaderHome"
 };
 </script>
-
 
 <style scoped lang="scss">
 .logo {
